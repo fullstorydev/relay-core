bd0a33f9a67d1bae320eb04e1d093ec130c26914
Trevor F. Smith <trevor@trevor.smith.name>
2020-01-21 10:00:55 -0800

    Remove blank test sources.

--- go/src/relay/plugins/plugin_test.go
+++
@@ -1,9 +0,0 @@
-package plugins
-
-import (
-	"testing"
-)
-
-func TestFoo(t *testing.T) {
-	// TODO add actual tests
-}
--- go/src/relay/plugins/traffic/logging/main/logging_test.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"testing"
-)
-
-func TestFoo(t *testing.T) {
-	// TODO add actual tests
-}
